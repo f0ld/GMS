227c4826ee00d8ef6310dd8a322680000e3ab1ae
f0ld <ewig.michael@gmail.com>
2026-04-03 23:27:31 +0200

    Added headless start of browser and http basic auth

--- gms.py
+++ gms.py
@@ -10,15 +10,26 @@ def load_config():
         return json.load(config_file)
 
 def main():
+        print("loading config")
         config = load_config()
         start_url = config["start_url"]
 
         pdf_files = [] #This will save the temporary pdf files later on
         page_num = 1
-        
 
 
+        print("starting Browser...")
+        with sync_playwright() as playwright:
+            browser = playwright.chromium.launch(headless=True) #starting Browser headless in the background
 
+            #HTTP Basic Auth config (Hopefully this will work):
+            context = browser.new_context(
+                http_credentials={"username": config["username"], "password": config["password"]}
+            )
+
+        page = context.new_page()
+        print(f"Navigate to: {start_url}")
+        page.goto(start_url)
 
 
 
