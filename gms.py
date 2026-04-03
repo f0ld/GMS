c79d95bd60aeb6d508ec749885ee49cdc757adf5
f0ld <ewig.michael@gmail.com>
2026-04-04 00:21:55 +0200

    Fixed a bug where the script didnt recognize the correct button class for the next button. Fixed a bug where the indentation was off leading to playwright crashing

--- gms.py
+++ gms.py
@@ -24,39 +24,40 @@ def main():
 
             #HTTP Basic Auth config (Hopefully this will work):
             context = browser.new_context(
-                http_credentials={"username": config["username"], "password": config["password"]}
+                http_credentials={"username": config["username"], "password": config["password"]},
+                ignore_https_errors=True
             )
 
-        page = context.new_page()
-        print(f"Navigate to: {start_url}")
-        page.goto(start_url)
+            page = context.new_page()
+            print(f"Navigate to: {start_url}")
+            page.goto(start_url)
 
-        while True:
-            page.wait_for_load_state("networkidle") #Wait for everything to load because the Math Stuff needs bandwidth
-            time.sleep(2) #Short Buffer to wait for JavasSript-Render (maybe needs an increase...)
+            while True:
+                page.wait_for_load_state("networkidle") #Wait for everything to load because the Math Stuff needs bandwidth
+                time.sleep(0.5) #Short Buffer to wait for JavasSript-Render (maybe needs an increase...)
 
-            #Save Page as PDF
-            pdf_filename = f"temp_page_{page_num:03d}.pdf"
-            page.pdf(
-                path=pdf_filename,
-                format = "A4",
-                print_background = True,
-                margin = {"top": "1cm", "bottom": "1cm", "left": "1cm", "right": "1cm"},
-            )
-            pdf_files.append(pdf_filename)
-            print(f"Saved: Page {page_num} ({page.url}")
+                #Save Page as PDF
+                pdf_filename = f"temp_page_{page_num:03d}.pdf"
+                page.pdf(
+                    path=pdf_filename,
+                    format = "A4",
+                    print_background = True,
+                    margin = {"top": "1cm", "bottom": "1cm", "left": "1cm", "right": "1cm"},
+                )
+                pdf_files.append(pdf_filename)
+                print(f"Saved: Page {page_num} ({page.url}")
 
-            #seach for next page button (might need to check if MySt standard classes are used)
-            next_button = page.locator("a.right-next, a.next-page, a.next").first
-            if next_button.count() > 0 and next_button.is_visible():
-                print("Jumping to next page...")
-                next_button.click()
-                page_num += 1
-            else:
-                print("No Next Button found. End of Script reached...")
-                break
+                #seach for next page button (might need to check if MySt standard classes are used)
+                next_button = page.locator("a.myst-footer-link-next").first
+                if next_button.count() > 0 and next_button.is_visible():
+                    print("Jumping to next page...")
+                    next_button.click()
+                    page_num += 1
+                else:
+                    print("No Next Button found. End of Script reached...")
+                    break
 
-        browser.close()
+            browser.close()
 
         print("\nMerging PDFs...")
         merger = PdfWriter()
@@ -66,7 +67,9 @@ def main():
         final_filename = "MyScript_Komplett.pdf"
         merger.write(final_filename)
         merger.close()
-        
+
+
+
         #CleanUp
         print("Cleaning up temporary files...")
         for pdf in pdf_files:
