a85772983c6af5acf52b8df48b766e850464e0bd
f0ld <ewig.michael@gmail.com>
2026-04-03 23:21:01 +0200

    Implemented basic load config helper function and started with variable definitions and main in general

--- gms.py
+++ gms.py
@@ -4,10 +4,22 @@ import os
 from playwright.sync_api import sync_playwright
 from pypdf import PdfWriter
 
-
+#Little helper to load config.json
+def load_config():
+    with open("config.json", "r") as config_file:
+        return json.load(config_file)
 
 def main():
-        pass
+        config = load_config()
+        start_url = config["start_url"]
+
+        pdf_files = [] #This will save the temporary pdf files later on
+        page_num = 1
+        
+
+
+
+
 
 
 if __name__ == "__main__":
