ea5bdac3fa0125757288b826b4f3230b50ea1daf
f0ld <ewig.michael@gmail.com>
2026-04-03 23:13:15 +0200

    Added Basic Script file. Added .gitignore and included pycharm files there

---
+++ gms.py
@@ -0,0 +1,14 @@
+import json
+import time
+import os
+from playwright.sync_api import sync_playwright
+from pypdf import PdfWriter
+
+
+
+def main():
+        pass
+
+
+if __name__ == "__main__":
+    main()
